b1b5cc178f6b3a240b7e0e6af0412dde694afbc2
colburncodes <colburnsanders@gmail.com>
2022-09-28 20:01:48 -0500

    data object

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+let initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "../images/yosemite-valley.png",
+  },
+  {
+    name: "Lake Louise",
+    link: "../images/lake-louise.png",
+  },
+  {
+    name: "Bald Mountains",
+    link: "../images/bald-mountains.png",
+  },
+  {
+    name: "Latemar",
+    link: "../images/latemar.png",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "../images/vanoise-national-park.png",
+  },
+  {
+    name: "Lago di Braies",
+    link: "../images/lago.png",
+  },
+];
