e69f976237b9ae4d87627bd176062bf1035fdf9a
colburncodes <colburnsanders@gmail.com>
2022-09-30 22:13:42 -0500

    updated image links

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,26 @@
-let initialCards = [
+const initialCards = [
   {
     name: "Yosemite Valley",
-    link: "../images/yosemite-valley.png",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
   },
   {
     name: "Lake Louise",
-    link: "../images/lake-louise.png",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
   },
   {
     name: "Bald Mountains",
-    link: "../images/bald-mountains.png",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
   },
   {
     name: "Latemar",
-    link: "../images/latemar.png",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
   },
   {
     name: "Vanoise National Park",
-    link: "../images/vanoise-national-park.png",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
   },
   {
     name: "Lago di Braies",
-    link: "../images/lago-di-braies.png",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
   },
 ];
