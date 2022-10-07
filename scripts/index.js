98a4555b171e515f81ee7c9dd69707f856018ab1
colburncodes <colburnsanders@gmail.com>
2022-10-07 18:47:30 -0500

    user can save profile

--- scripts/index.js
+++ scripts/index.js
@@ -25,22 +25,55 @@ const initialCards = [
   },
 ];
 
-// Buttons 
+// Buttons
 const profileEditButton = document.querySelector(".profile__edit-button");
 const modalCloseButton = document.querySelector(".modal__close-button");
 
 // Modal
 const profileEditModal = document.querySelector(".modal");
+const profileModalForm = document.querySelector(".modal__form");
+// Profile
+const profileTitleInput = document.querySelector(".profile__title");
+const profileDescriptionInput = document.querySelector(".profile__description");
+const profileTitleEl = profileEditModal.querySelector(".modal__input-name");
+const profileDescriptionEl = profileEditModal.querySelector(
+  ".modal__input-description"
+);
+// Template
+let cardTemplate = document.querySelector("#card-template").content;
+let cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+console.log(cardElement);
+
+openModal = () => {
+  profileEditModal.classList.add("modal__open");
+};
+
+closeModal = () => {
+  profileEditModal.classList.remove("modal__open");
+};
+
 
 // Open Modal
 profileEditButton.addEventListener("click", () => {
-  profileEditModal.classList.add('modal__open');
+  profileTitleEl.value = profileTitleInput.textContent;
+  profileDescriptionEl.value = profileDescriptionInput.textContent;
+  openModal();
 });
 
 // Close Modal
-modalCloseButton.addEventListener("click", () => {
-  profileEditModal.classList.remove('modal__open');
-})
+modalCloseButton.addEventListener("click", closeModal);
 
+profileModalForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const titleValue = evt.target.title.value;
+  const descriptionValue = evt.target.description.value;
 
+  if (titleValue !== "" || descriptionValue !== "") {
+    profileTitleInput.textContent = titleValue;
+    profileDescriptionInput.textContent = descriptionValue;
+  }
+  closeModal();
+});
 
+getCardElement = (data) => {};
