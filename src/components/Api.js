db100b15e53187448fb71a75227319ba69e13cbd
colburncodes <colburnsanders@gmail.com>
2022-12-18 09:47:44 -0600

    more functionality to api/refactored index

--- src/components/Api.js
+++ src/components/Api.js
@@ -15,6 +15,45 @@ class Api {
     return data;
   };
 
+  // POST: Add new card
+  addCard = async ({ name, link }) => {
+    let response = await fetch(`${this._baseUrl}/cards`, {
+      method: "POST",
+      headers: {
+        authorization: this._authToken,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        link,
+      }),
+    });
+
+    let data = response.ok
+      ? response.json()
+      : Promise.reject(`Error: ${response.status} ${response.statusText}`);
+    return data;
+  };
+
+  // DELETE: Delete card
+  deleteCardById = async (id) => {
+    let response = await fetch(`${this._baseUrl}/cards/${id}`, {
+      method: "DELETE",
+      headers: {
+        authorization: this._authToken,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id,
+      }),
+    });
+
+    let data = response.ok
+      ? response.json()
+      : Promise.reject(`Error: ${response.status} ${response.statusText}`);
+    return data;
+  };
+
   // GET: Gets User Profile
   getUserInfo = async () => {
     let response = await fetch(`${this._baseUrl}/users/me`, {
@@ -27,7 +66,7 @@ class Api {
   };
 
   // PATCH: Edit User Profile
-  updateUserProfile = async ({ name, about }) => {
+  editUserInfo = async ({ name, about }) => {
     let response = await fetch(`${this._baseUrl}/users/me`, {
       method: "PATCH",
       headers: {
